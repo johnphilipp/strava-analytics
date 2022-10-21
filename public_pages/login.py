3d16694c690e3132046415622fa26a857bad5b02
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-10-21 16:44:57 +0200

    Changed home button

--- public_pages/login.py
+++ public_pages/login.py
@@ -4,5 +4,9 @@ from auth.auth import get_authorization_url
 
 
 def login():
-    link = '[Login and Authorize]({})'.format(get_authorization_url())
+    with open("style/style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+    link = """<a href="{}" class="button">Login and Authenticate</a>""".format(
+        get_authorization_url())
     st.markdown(link, unsafe_allow_html=True)
