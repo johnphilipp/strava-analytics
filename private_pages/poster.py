0a54874fcb6413a4e6662717a122fd87be6851eb
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-10-25 17:26:25 +0200

    debugging df

--- private_pages/poster.py
+++ private_pages/poster.py
@@ -27,6 +27,9 @@ def poster(df):
     # Grid size
     size_selected = st.radio(
         "Select poster size", ("3x3", "5x5", "10x10", "20x20", "5x20"))
+
+    st.dataframe(df)
+
     if size_selected == "3x3":
         specs = {"len": 9, "w": 1800, "h": 1800, "wh_single": 600}
         if specs["len"] > len(df):
@@ -35,6 +38,7 @@ def poster(df):
         else:
             st.image(collage_fig(df, map_style_selected,
                      specs, line_color, line_thickness))
+            st.dataframe(df)
     elif size_selected == "5x5":
         specs = {"len": 25, "w": 2000, "h": 2000, "wh_single": 400}
         if specs["len"] > len(df):
@@ -43,6 +47,7 @@ def poster(df):
         else:
             st.image(collage_fig(df, map_style_selected,
                      specs, line_color, line_thickness))
+            st.dataframe(df)
     elif size_selected == "10x10":
         specs = {"len": 100, "w": 2500, "h": 2500, "wh_single": 250}
         if specs["len"] > len(df):
@@ -51,6 +56,7 @@ def poster(df):
         else:
             st.image(collage_fig(df, map_style_selected,
                      specs, line_color, line_thickness))
+            st.dataframe(df)
     elif size_selected == "20x20":
         specs = {"len": 400, "w": 2000, "h": 2000, "wh_single": 100}
         if specs["len"] > len(df):
@@ -59,6 +65,7 @@ def poster(df):
         else:
             st.image(collage_fig(df, map_style_selected,
                      specs, line_color, line_thickness))
+            st.dataframe(df)
     elif size_selected == "5x20":
         specs = {"len": 50, "w": 2000, "h": 1000, "wh_single": 200}
         if specs["len"] > len(df):
@@ -67,6 +74,7 @@ def poster(df):
         else:
             st.image(collage_fig(df, map_style_selected,
                      specs, line_color, line_thickness))
+            st.dataframe(df)
 
 
 if __name__ == "__main__":
