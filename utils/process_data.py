bf761f6301ff82c16fbdeb42f5e1c67740006dae
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-10-25 17:15:08 +0200

    modified process_data to handle keys correctly for data from api

--- utils/process_data.py
+++ utils/process_data.py
@@ -14,27 +14,47 @@ def process_data(df):
     """
     Return modified df of activities
     """
-    df = df[["name",
-             "distance",
-             "moving_time",
-             "type",
-             "start_date_local",
-             "achievement_count",
-             "kudos_count",
-             "start_latlng",
-             "end_latlng",
-             "average_heartrate",
-             "total_elevation_gain",
-             "map"]]
-    df[["start_lat", "start_lng"]] = pd.DataFrame(
-        df.start_latlng.tolist(), index=df.index)
-    df[["end_lat", "ent_lng"]] = pd.DataFrame(
-        df.end_latlng.tolist(), index=df.index)
-    df = df[~df["map"].isnull()]
-    df[["id", "summary_polyline", "resource_state"]] = pd.DataFrame(
-        df.map.tolist(), index=df.index)
-    df = df.drop(columns=["start_latlng", "end_latlng",
-                 "map", "id", "resource_state"])
-    df["summary_polyline"] = df["summary_polyline"].apply(
-        lambda x: polyline.decode(x))
+    if "map" in df.keys():
+        df = df[["name",
+                "distance",
+                 "moving_time",
+                 "type",
+                 "start_date_local",
+                 "achievement_count",
+                 "kudos_count",
+                 "start_latlng",
+                 "end_latlng",
+                 "average_heartrate",
+                 "total_elevation_gain",
+                 "map"]]
+        df[["start_lat", "start_lng"]] = pd.DataFrame(
+            df.start_latlng.tolist(), index=df.index)
+        df[["end_lat", "ent_lng"]] = pd.DataFrame(
+            df.end_latlng.tolist(), index=df.index)
+        df = df[~df["map"].isnull()]
+        df[["id", "summary_polyline", "resource_state"]] = pd.DataFrame(
+            df.map.tolist(), index=df.index)
+        df = df.drop(columns=["start_latlng", "end_latlng",
+                              "map", "id", "resource_state"])
+        df["summary_polyline"] = df["summary_polyline"].apply(
+            lambda x: polyline.decode(x))
+    else:
+        df = df[["name",
+                 "distance",
+                 "moving_time",
+                 "type",
+                 "start_date_local",
+                 "achievement_count",
+                 "kudos_count",
+                 "start_latlng",
+                 "end_latlng",
+                 "average_heartrate",
+                 "total_elevation_gain",
+                 "map.summary_polyline"]]
+        df[["start_lat", "start_lng"]] = pd.DataFrame(
+            df.start_latlng.tolist(), index=df.index)
+        df[["end_lat", "ent_lng"]] = pd.DataFrame(
+            df.end_latlng.tolist(), index=df.index)
+        df["summary_polyline"] = df["map.summary_polyline"].apply(
+            lambda x: polyline.decode(x))
     return df
