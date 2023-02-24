2c1eac82c8f406c53e4c58231ae6a8fe090097cb
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2023-02-24 12:35:42 +0100

    fixed zoom center bug

--- pages/1_📍_Heatmap_Visualizer.py
+++ pages/1_📍_Heatmap_Visualizer.py
@@ -14,14 +14,13 @@ def get_single_lat_lng(df, i):
 
 def heatmap(df):
     types_available = list_options.activity_types()
-    type_selected = st.multiselect("Select activity types",
-                                   types_available,
-                                   default=types_available)
+    type_selected = st.multiselect(
+        "Select activity types", types_available, default=types_available
+    )
 
     df = df[df["type"].isin(type_selected)]
 
-    map_style_selected = st.selectbox("Select map style",
-                                      list_options.map_styles(), 0)
+    map_style_selected = st.selectbox("Select map style", list_options.map_styles(), 0)
 
     fig = heatmap_fig(df, map_style_selected)
 
--- utils/fig.py
+++ utils/fig.py
@@ -16,17 +16,36 @@ def _zoom_center(df, margin_selected):
     maxlon, minlon = max(lons), min(lons)
     maxlat, minlat = max(lats), min(lats)
     center = {
-        'lon': round((maxlon + minlon) / 2, 6),
-        'lat': round((maxlat + minlat) / 2, 6)
+        "lon": round((maxlon + minlon) / 2, 6),
+        "lat": round((maxlat + minlat) / 2, 6),
     }
 
     # longitudinal range by zoom level (20 to 1)
     # in degrees, if centered at equator
-    lon_zoom_range = np.array([
-        0.0007, 0.0014, 0.003, 0.006, 0.012, 0.024, 0.048, 0.096,
-        0.192, 0.3712, 0.768, 1.536, 3.072, 6.144, 11.8784, 23.7568,
-        47.5136, 98.304, 190.0544, 360.0
-    ])
+    lon_zoom_range = np.array(
+        [
+            0.0007,
+            0.0014,
+            0.003,
+            0.006,
+            0.012,
+            0.024,
+            0.048,
+            0.096,
+            0.192,
+            0.3712,
+            0.768,
+            1.536,
+            3.072,
+            6.144,
+            11.8784,
+            23.7568,
+            47.5136,
+            98.304,
+            190.0544,
+            360.0,
+        ]
+    )
 
     margin = margin_selected
     height = (maxlat - minlat) * margin * 2.0
@@ -39,20 +58,31 @@ def _zoom_center(df, margin_selected):
 
 
 @st.cache(max_entries=10)
-def line_fig(df, map_style_selected="white-bg", margin_selected=2, line_color="Black", line_thickness=20, height=500):
+def line_fig(
+    df,
+    map_style_selected="white-bg",
+    margin_selected=2,
+    line_color="Black",
+    line_thickness=20,
+    height=500,
+):
     """
     Return plotly map
     """
-    fig = px.line_mapbox(df,
-                         lat="start_lat",
-                         lon="start_lng",
-                         height=height,
-                         color_discrete_sequence=px.colors.qualitative.Set1)
+    fig = px.line_mapbox(
+        df,
+        lat="start_lat",
+        lon="start_lng",
+        height=height,
+        color_discrete_sequence=px.colors.qualitative.Set1,
+    )
     zoom, center = _zoom_center(df, margin_selected)
-    fig.update_layout(mapbox_style=map_style_selected,
-                      mapbox_zoom=zoom,
-                      mapbox_center=center,
-                      margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.update_layout(
+        mapbox_style=map_style_selected,
+        mapbox_zoom=zoom,
+        mapbox_center=center,
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+    )
     fig.update_traces(line=dict(color=line_color, width=line_thickness))
     return fig
 
@@ -62,18 +92,22 @@ def heatmap_fig(df, map_style_selected):
     """
     Return plotly heatmap
     """
-    fig = px.density_mapbox(df,
-                            lat='start_lat',
-                            lon='start_lng',
-                            radius=10,
-                            height=500,
-                            center=dict(lat=30, lon=0),
-                            zoom=1)
-    zoom, center = _zoom_center(df)
-    fig.update_layout(mapbox_style=map_style_selected,
-                      mapbox_zoom=zoom,
-                      mapbox_center=center,
-                      margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig = px.density_mapbox(
+        df,
+        lat="start_lat",
+        lon="start_lng",
+        radius=10,
+        height=500,
+        center=dict(lat=30, lon=0),
+        zoom=1,
+    )
+    zoom, center = _zoom_center(df, margin_selected=2)
+    fig.update_layout(
+        mapbox_style=map_style_selected,
+        mapbox_zoom=zoom,
+        mapbox_center=center,
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+    )
     return fig
 
 
@@ -101,10 +135,13 @@ def get_img(fig, crop=False):
 
 
 @st.cache(max_entries=5)
-def collage_fig(df, map_style_selected, margin_selected, specs, line_color, line_thickness):
+def collage_fig(
+    df, map_style_selected, margin_selected, specs, line_color, line_thickness
+):
     """
     Return a collage of size specs["len"] with cropped images of polylines
     """
+
     def _get_imgs(df):
         """
         Return list with images as bytes
@@ -113,26 +150,31 @@ def collage_fig(df, map_style_selected, margin_selected, specs, line_color, line
         for i in range(0, specs["len"]):
             single = _get_single_lat_lng(df, i)
             if len(single > 0):
-                fig = line_fig(single, map_style_selected, margin_selected,
-                               line_color, line_thickness, 700)
+                fig = line_fig(
+                    single,
+                    map_style_selected,
+                    margin_selected,
+                    line_color,
+                    line_thickness,
+                    700,
+                )
                 img = get_img(fig, crop=True)
                 imgs.append(img)
         return imgs
 
     def _get_collage(imgs, specs):
         """
-        Return final collage 
+        Return final collage
         """
-        collage = Image.new("RGBA",
-                            (specs["w"], specs["h"]),
-                            color=(255, 255, 255, 255))
+        collage = Image.new(
+            "RGBA", (specs["w"], specs["h"]), color=(255, 255, 255, 255)
+        )
         c = 0
         for i in range(0, specs["h"], specs["wh_single"]):
             for j in range(0, specs["w"], specs["wh_single"]):
                 if c < len(imgs):
                     photo = imgs[c].convert("RGBA")
-                    photo = photo.resize(
-                        (specs["wh_single"], specs["wh_single"]))
+                    photo = photo.resize((specs["wh_single"], specs["wh_single"]))
 
                     collage.paste(photo, (j, i))
                     c += 1
@@ -145,10 +187,10 @@ def collage_fig(df, map_style_selected, margin_selected, specs, line_color, line
 
 
 def invert_colors_collage(collage_fig):
-    return ImageOps.invert(collage_fig.convert('RGB'))
+    return ImageOps.invert(collage_fig.convert("RGB"))
 
 
 def invert_colors_img(img):
     img = Image.open(BytesIO(img))
-    img = ImageOps.invert(img.convert('RGB'))
+    img = ImageOps.invert(img.convert("RGB"))
     return img
